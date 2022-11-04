66c7cc22a5ed48b4e9341e2128733340b3974067
LucasCscBks <lucas_reisgod@yahoo.com.br>
2022-11-03 22:45:26 -0300

    exercicio 3 typescript

--- Exercício1_TypeScript/dist/index.js
+++ Exercício1_TypeScript/dist/index.js
@@ -52,9 +52,44 @@ class BooleanValidator extends Validator {
         }
     }
 }
-let stringError = new StringValidator(22);
-let numberError = new NumberValidator(true);
-let booleanError = new BooleanValidator('teste');
-let string = new StringValidator('Teste correto');
-let number = new NumberValidator(77);
-let boolean = new BooleanValidator(true);
+// let stringError = new StringValidator(22);
+// let numberError = new NumberValidator(true);
+// let booleanError = new BooleanValidator('teste');
+// let string = new StringValidator('Teste correto');
+// let number = new NumberValidator(77);
+// let boolean = new BooleanValidator(true);
+class RegexValidator extends StringValidator {
+    constructor(data) {
+        try {
+            super(data);
+            let re = /^(\w{1,}@\w{1,}\.(\w{3})(\.\w{2}){0,1})$/gim;
+            let ok = re.exec(data);
+            if (ok === null) {
+                throw new Error(`O formato está errado`);
+            }
+            else {
+                console.log(`${data} : está no formado correto.`);
+            }
+        }
+        catch (err) {
+            console.log(err);
+        }
+    }
+}
+class EmailInput extends HTMLElement {
+    constructor() {
+        super();
+        console.log('classe instanciada');
+        const shadow = this.attachShadow({ mode: "closed" });
+        this.emailInput = document.createElement('input');
+        this.emailInput.type = 'email';
+        this.emailInput.style.border = 'none';
+        this.emailInput.onchange = (e) => this.onChangedEmail(e);
+        shadow.appendChild(this.emailInput);
+    }
+    onChangedEmail(e) {
+        console.log(this.emailInput.value);
+        let test = new RegexValidator(this.emailInput.value);
+    }
+}
+customElements.define("email-input", EmailInput);
